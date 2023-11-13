8e4c5c2b962d55eec501d1d32c9f234feaa49532
maxgallup <maxgallup@pm.me>
2023-11-13 15:49:06 +0100

    port for gnome 45

---
+++ tailscale-status@maxgallup.github.com/customUtils.js
@@ -0,0 +1,10 @@
+
+
+export function myWarn(string) {
+    log("🟡 [tailscale-status]: " + string);
+}
+
+export function myError(string) {
+    log("🔴 [tailscale-status]: " + string);
+}
+
--- tailscale-status@maxgallup.github.com/prefs.js
+++ tailscale-status@maxgallup.github.com/prefs.js
@@ -1,59 +1,59 @@
-// const { Adw, Gio, Gtk } = imports.gi;
-import Gio from 'gi://Gio';
-import Gtk from 'gi://Gtk';
+
 import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
 
-// const ExtensionUtils = imports.misc.extensionUtils;
-import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
-const Me = ExtensionUtils.getCurrentExtension();
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-
-function init() {
-}
-
-function fillPreferencesWindow(window) {
- 
-    const settings = ExtensionUtils.getSettings("org.gnome.shell.extensions.tailscale-status")
-   
-    log(settings)
- 
-    const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup();
-    page.add(group);
-
- 
-    const firstRow = new Adw.ActionRow({ title: 'Refresh time (seconds)' });
-    group.add(firstRow);
-
-    let spinButton = new Gtk.SpinButton({
-             value: settings.get_int ('refresh-interval'),
-             valign: Gtk.Align.CENTER,
-         });
-    spinButton.set_sensitive(true);
-    spinButton.set_range(0, 120);
-    spinButton.set_increments(1, 2);
-    spinButton.set_value(settings.get_int ('refresh-interval'));
-    spinButton.connect("value-changed", function (w) {
-      
-        settings.set_int ('refresh-interval',w.get_value_as_int())
-      });
-    
-     
-   
-    firstRow.add_suffix(spinButton);
-    firstRow.activatable_widget = spinButton;
-    const secondRow = new Adw.ActionRow({ title: 'Login-Server URL' });
-    group.add(secondRow);
-    const textBox = new Gtk.Entry({
-        buffer: new Gtk.EntryBuffer()
-    });
-    textBox.set_text(settings.get_string ('login-server'));
-    textBox.connect("changed", function (w) {
-        // .get_buffer().text
-        settings.set_string('login-server',w.get_buffer().text)
+export default class TailscaleStatusExtensionPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
         
-      });
-    secondRow.add_suffix(textBox);
+        log(" >>> in settings");
 
-    window.add(page);
+        window._settings = this.getSettings();
+        let settings = this.getSettings();
+ 
+        const page = new Adw.PreferencesPage();
+        const group = new Adw.PreferencesGroup();
+
+        page.add(group);
+
+        const firstRow = new Adw.ActionRow({ title: 'Refresh time (seconds)' });
+        group.add(firstRow);
+    
+        let spinButton = new Gtk.SpinButton({
+                value: settings.get_int ('refresh-interval'),
+                valign: Gtk.Align.CENTER,
+             });
+        spinButton.set_sensitive(true);
+        spinButton.set_range(0, 120);
+        spinButton.set_increments(1, 2);
+        spinButton.set_value(settings.get_int ('refresh-interval'));
+        spinButton.connect("value-changed", function (w) {
+          
+            settings.set_int ('refresh-interval',w.get_value_as_int())
+          });
+       
+        firstRow.add_suffix(spinButton);
+        firstRow.activatable_widget = spinButton;
+        const secondRow = new Adw.ActionRow({ title: 'Login-Server URL' });
+        group.add(secondRow);
+        const textBox = new Gtk.Entry({
+            buffer: new Gtk.EntryBuffer()
+        });
+        textBox.set_text(settings.get_string ('login-server'));
+        textBox.connect("changed", function (w) {
+            // .get_buffer().text
+            settings.set_string('login-server',w.get_buffer().text)
+            
+          });
+        secondRow.add_suffix(textBox);
+    
+        window.add(page);
+    }
 }
+
+
+
+
+
+
