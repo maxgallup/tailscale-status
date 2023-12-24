6af4647429a166fdf50ce66f2ffff736d66c3375
maxgallup <maxgallup@pm.me>
2023-12-24 11:29:50 +0100

    bug fix

--- tailscale-status@maxgallup.github.com/extension.js
+++ tailscale-status@maxgallup.github.com/extension.js
@@ -134,9 +134,9 @@ function sortNodes(a, b) {
 }
 
 function sortByName(a, b) {
-    if (a.name[0] > b.name[0]) {
+    if (a > b) {
         return 1;
-    } else if (a.name[0] == b.name[0]) {
+    } else if (a == b) {
         return 0;
     } else {
         return -1;
